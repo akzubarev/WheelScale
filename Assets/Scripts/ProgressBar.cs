398cd8d62b8622d6454f787777785c5a8e6e7424
Alex <akzubarev@edu.hse.ru>
2020-05-28 11:58:35 +0300

    Added progress bar

---
+++ Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+
+    public Transform player, finishLine;
+    
+    void Update()
+    {
+        GetComponent<Slider>().value=player.position.z/finishLine.position.z;     
+    }
+}
